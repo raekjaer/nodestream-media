93413edb3a15c22a5323cf89f4cd9cbb73086493
Fabian Sorqvist <fabian.sorqvist@gmail.com>
2012-08-14 16:03:10 +0200

    Add some users

--- configdump/content/users_roles.php
+++ configdump/content/users_roles.php
@@ -1,7 +1,24 @@
 <?php
 function configdump_table_users_roles() {
   return array(
-    'data' => array(),
+    'data' => array(
+      0 => (object) array(
+        'uid' => '5',
+        'rid' => '6',
+      ),
+      1 => (object) array(
+        'uid' => '6',
+        'rid' => '9',
+      ),
+      2 => (object) array(
+        'uid' => '4',
+        'rid' => '10',
+      ),
+      3 => (object) array(
+        'uid' => '6',
+        'rid' => '10',
+      ),
+    ),
     'serialized_cols' => array(),
   );
 }
